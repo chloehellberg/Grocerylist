513767d37e06cdfbb8a63019d3616036381c0cf9
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-17 15:44:18 -0700

    fixed JS to print un oreded list of items
    Co-authored by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,14 @@ $(document).ready(function() {
     let items = ["item1","item2", "item3", "item4" ,"item5" ,"item6"];
     items.forEach(function(input) {
       let listItems =  $("input#" + input).val();
-      $("." + input).text(listItems);
-      // $("#groceries").append("<li>" + input + "</li>");
+      // $("#" + input).text(listItems);
+      $("#groceries").append("<li>" + listItems + "</li>");
     });
     $("#shoppingList").show();
     event.preventDefault();
     
   });
 });    
-
-
-
 
 // $(document).ready(function() {
 //   const icecream = ["chocolate", "vanilla", "rocky road", "marionberry"];
@@ -23,3 +20,16 @@ $(document).ready(function() {
 //     }); 
 //   });
   
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+    
+//     let list1 = ["person1","person2", "animal", "exclamation" ,"verb" ,"noun"];
+//     list1.forEach(function(input) {
+//       let story =  $("input#" + input).val();
+//       $("." + input).text(story);
+//     });
+//     $("#arrays").show();
+//     event.preventDefault();
+    
+//   });
+// });  
